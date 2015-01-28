db9af906f65c2e76bfa37a96611942e300a08a18
Carlos Asensio Martínez <casensio@gmail.com>
2015-01-29 00:12:12 +0100

    Ajustamos la base de datos de sectores de Alicante. Eliminamos los textos iniciales.

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -3,7 +3,6 @@ include_once "lib/functions.php";
 
 if(isset($_POST["email_login"]) && isset($_POST["pass_login"]))
 {
-	$email = $_POST["email_login"];
 	$email = addslashes($_POST["email_login"]);
 	$pass  = $_POST["pass_login"];
 	

6b8446c9383636cef163195023dc651e1e7407f4
Carlos Asensio Martínez <casensio@gmail.com>
2015-02-06 16:55:56 +0100

    Obtenemos el listado de ejes programáticos para las jornadas participativas presenciales. Mostramos la gente inscrita en dichas jornadas.

--- jornadas.php
+++ jornadas.php
@@ -5,7 +5,10 @@ if(isset($_SESSION["usuario"]) && JornadasActivas())
 {   
     $usuario      = autentificado();
     $user_id      = array('id' => $usuario['id']);
-    $jornadas     = "SELECT * FROM prog_jornadas";
+    $jornadas     = "SELECT * FROM prog_sectores";
+    $jornadas     = "SELECT s.id, s.nombre, count(uj.usuario_id) as total FROM `prog_sectores` s 
+                        LEFT JOIN prog_users_jornada uj ON uj.jornada_id = s.id
+                        GROUP BY s.id, s.nombre";
     $jornada_user = "SELECT * FROM prog_users_jornada WHERE usuario_id=:id";
     
     $datos = array(
